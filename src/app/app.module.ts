92a9ab17f9f4d0c1d8367a3afa09ebaf0fc8442c
Yeu Ho <hothiyeu.dev@gmail.com>
2024-07-12 16:22:11 +0700

    Remove code unused

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,11 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AddTutorialComponent } from './components/add-tutorial/add-tutorial.component';
-import { TutorialDetailsComponent } from './components/tutorial-details/tutorial-details.component';
-import { TutorialsListComponent } from './components/tutorials-list/tutorials-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AddTutorialComponent,
-    TutorialDetailsComponent,
-    TutorialsListComponent
+    
   ],
   imports: [
     BrowserModule,
